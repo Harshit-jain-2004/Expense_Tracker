c1eac0ac6ecded4b0bbbbd90d41f8fc8151d6137
Harshit-jain-2004 <harshitjain20apr@gmail.com>
2024-08-21 23:13:25 +0530

    Schema

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const app = express()
+const {readdirSync} = require('fs')
 const {db} = require('../db/db.js')
 
 require('dotenv').config()
@@ -10,6 +11,9 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+//routes
+readdirSync('./').map((route)=>app.use('/api/v1/', require('./transactions.js')))
 
 
 const server = () => {
